4913c026fe65414cdeb00e258f096bc7dde263e2
Wakhi-Ken <w.dlamini@gmail.com>
2025-11-03 15:48:05 +0200

    Xml documentation done

--- Stopwatch_Application.Tests/Stopwatch_Application.Tests.cs
+++ Stopwatch_Application.Tests/Stopwatch_Application.Tests.cs
@@ -2,16 +2,28 @@ using NUnit.Framework;
 
 namespace Stopwatch_Application.Tests
 {
+    /// <summary>
+    /// Unit tests for the <see cref="StopwatchLogic"/> class.
+    /// </summary>
     public class StopwatchLogicTests
     {
+        /// <summary>
+        /// The stopwatch instance under test.
+        /// </summary>
         private StopwatchLogic stopwatch;
 
+        /// <summary>
+        /// Creates a new stopwatch before each test.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
             stopwatch = new StopwatchLogic();
         }
 
+        /// <summary>
+        /// Starting should reset time and set running state.
+        /// </summary>
         [Test]
         public void Start_ShouldResetTimeAndStartRunning()
         {
@@ -20,6 +32,9 @@ namespace Stopwatch_Application.Tests
             Assert.IsTrue(stopwatch.IsRunning);
         }
 
+        /// <summary>
+        /// A single tick should increase seconds by one.
+        /// </summary>
         [Test]
         public void Tick_ShouldIncreaseSeconds_WhenRunning()
         {
@@ -28,6 +43,9 @@ namespace Stopwatch_Application.Tests
             Assert.AreEqual("00:00:01", stopwatch.GetFormattedTime());
         }
 
+        /// <summary>
+        /// Ticks should roll over correctly to minutes and hours.
+        /// </summary>
         [Test]
         public void Tick_ShouldRollOverMinutesAndHours()
         {
@@ -36,6 +54,9 @@ namespace Stopwatch_Application.Tests
             Assert.AreEqual("01:01:01", stopwatch.GetFormattedTime());
         }
 
+        /// <summary>
+        /// Pausing should stop time progression.
+        /// </summary>
         [Test]
         public void Pause_ShouldStopTimeProgress()
         {
@@ -46,6 +67,9 @@ namespace Stopwatch_Application.Tests
             Assert.AreEqual("00:00:01", stopwatch.GetFormattedTime());
         }
 
+        /// <summary>
+        /// Resuming should continue counting from paused time.
+        /// </summary>
         [Test]
         public void Resume_ShouldContinueCounting()
         {
@@ -57,6 +81,9 @@ namespace Stopwatch_Application.Tests
             Assert.AreEqual("00:00:02", stopwatch.GetFormattedTime());
         }
 
+        /// <summary>
+        /// Reset should zero time and stop the stopwatch.
+        /// </summary>
         [Test]
         public void Reset_ShouldReturnToZero()
         {
@@ -67,6 +94,9 @@ namespace Stopwatch_Application.Tests
             Assert.IsFalse(stopwatch.IsRunning);
         }
 
+        /// <summary>
+        /// Stop should prevent further time advancement.
+        /// </summary>
         [Test]
         public void Stop_ShouldStopCounting()
         {
